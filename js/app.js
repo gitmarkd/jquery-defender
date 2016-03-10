61f8214ac068aa497c0566eb83bee70c12154d4c
Mark Daniels <github.markdaniels@gmail.com>
2016-03-09 18:27:14 -0800

    html and css setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*
+Page loads
+Ryu is standing still
+Mouseenter  Ryu - shifts to ready pose
+Mouseleave - return to still
+Mousedown, leans forward and throws a Hadouken
+Remains in pose until Mouseup - return to his ready pose
+Keydown "x", Ryu assumes "looking cool" pose
+Keyup "x" (keyup) - returns to ready pose
+*/
